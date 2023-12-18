3a7aaaaba026ba28c4c367e0d81a0e985fe9a37d
angelfr10 <angeelfr.10@gmail.com>
2023-12-18 20:10:42 +0100

    RANKING

--- ranking.py
+++ ranking.py
@@ -1,16 +1,19 @@
+import datetime
 class NodoRanking:
-    def __init__(self, nombre, puntuacion):
+    def __init__(self, nombre, puntuacion, oponente, fecha = None):
         self.nombre = nombre
         self.puntuacion = puntuacion
         self.anterior = None
         self.siguiente = None
+        self.oponente = oponente
+        self.fecha = fecha if fecha else datetime.datetime.now()
 
 class ListaDoblementeEnlazada:
     def __init__(self):
         self.cabeza = None
         self.cola = None
 
-    def insertar_ordenado(self, nombre, puntuacion):
+    def insertar_ordenado(self, nombre, puntuacion,oponente):
         nuevo_nodo = NodoRanking(nombre, puntuacion)
         if not self.cabeza or nuevo_nodo.puntuacion >= self.cabeza.puntuacion:
             nuevo_nodo.siguiente = self.cabeza
@@ -33,6 +36,6 @@ class ListaDoblementeEnlazada:
         ranking = ""
         actual = self.cabeza
         while actual:
-            ranking += f"{actual.nombre}: {actual.puntuacion}\n"
+            ranking += f"{actual.nombre}: {actual.puntuacion}, Oponente: {actual.oponente}, Fecha: {actual.fecha.strftime('%Y-%m-%d %H:%M:%S')}\n"
             actual = actual.siguiente
         return ranking
